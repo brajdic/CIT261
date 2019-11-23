8f1451f11c2a454e548d28fd908ca34ee949be51
brajdic <32497664+brajdic@users.noreply.github.com>
2019-11-22 19:51:01 -0500

    Add files via upload

--- tests/Designing, Defining, and Triggering CSS3 Transforms without Custom Libraries (Thought Library)/index.php
+++ tests/Designing, Defining, and Triggering CSS3 Transforms without Custom Libraries (Thought Library)/index.php
@@ -1,13 +1,16 @@
-<h1>Designing, Defining, and Triggering CSS3 Transforms without Custom Libraries (Thought Library)</h1>
+<h1>Designing, Defining, and Triggering CSS3 Transforms without Custom Libraries (Thought Library)</h1><br><br>
+<h4>The following Transforms were done completely in CSS; no JavaScript used.</h4><br>
+<div>Hover over the corners of the triangle to roate the object by 180 degress and scew the X axis by 20 degrees.</div><br>
 <div>
-
-
-
-
-
-
-
-
-
+<div class="transform2">
+	<div class="transform1"></div>
+</div><br><br>
+Click and hold the video to rotate it by 180 degrees.<br><br>
+<video class="transformVideo" controls>
+  <source src="/cit261/videos/hikarunara.mp4" type="video/mp4">
+  <source src="/cit261/videos/hikarunara.mp4" type="ogg/mp4">
+  Your browser does not support the video element. :/
+</video>
+<br><br>
 </div>
 <?php include("view/footer.php");?>
